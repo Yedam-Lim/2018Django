a248a27d3a96c9867a8b3bd8b357ec0d431f93f4
Yedam Lim <damy0331@gmail.com>
2018-01-10 15:43:31 +0900

    second server api added -mysql,postmethod override needed

--- rcapp/urls.py
+++ rcapp/urls.py
@@ -4,8 +4,19 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     url(r'^api-token-auth/', obtain_auth_token),
-    url(r'^recordings/$', views.RecordingList),
-    url(r'^recordings/(?P<pk>[0-9]+)/$', views.RecordingDetail),
-    url(r'^users/$', views.UserList),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail),
+    url(r'^recordings/$',
+        views.RecordingList.as_view(),
+        name=views.RecordingList.name),
+    url(r'^recordings/(?P<pk>[0-9]+)/$',
+        views.RecordingDetail.as_view(),
+        name=views.RecordingDetail.name),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name=views.UserList.name),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name=views.UserDetail.name),
+    url(r'^$',
+        views.ApiRoot.as_view(),
+        name=views.ApiRoot.name),
 ]
